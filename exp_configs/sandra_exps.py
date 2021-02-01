26586b20b6e12b2c03a06a98be119e068e5651b6
liuzheyu1998 <zheyu.liu@mail.mcgill.ca>
2021-02-01 15:38:40 +0800

    Update sandra_exps.py

--- exp_configs/sandra_exps.py
+++ exp_configs/sandra_exps.py
@@ -43,6 +43,7 @@ EXP_GROUPS['bitempered'] = hu.cartesian_exp_group({
                         'batch_size': 32,
                         'opt': {'name': 'adamW', 'lr': 0.0001, 'wd': 1e-6},
                         'model': {'name': 'resnext50_32x4d_ssl'},
+                        'loss_func': {'name': 'bitempered', 't1': 1, 't2': 1, 'reduction': 'mean'},
                         'max_epoch': [30]
                         })
 
