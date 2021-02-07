d3166ab7f4ccbc12782a137ae7df78543dbe8450
Xinhong Deng <sandra.deng.0520@gmail.com>
2021-02-06 22:43:26 -0500

    fix sam

--- src/models/standard.py
+++ src/models/standard.py
@@ -100,8 +100,6 @@ def get_optimizer(exp_dict, model):
         )
     elif opt_dict['name'] == 'adam':
         return torch.optim.Adam(model.parameters(), lr=opt_dict['lr'])
-    elif opt_dict['name'] == 'sam':
-        return SAM(model.parameters())
     else:
         return torch.optim.Adam(model.parameters(), lr=opt_dict['lr'])
 
@@ -488,60 +486,3 @@ def tempered_softmax(activations, t, num_iters=5):
 
     normalization_constants = compute_normalization(activations, t, num_iters)
     return exp_t(activations - normalization_constants, t)
-
-
-class SAM(torch.optim.Optimizer):
-    def __init__(self, params, rho=0.05, **kwargs):
-        base_optimizer = torch.optim.SGD
-        default = dict(rho = rho, **kwargs)
-        super(SAM, self).__init__(params, default)
-
-        self.base_optimizer = base_optimizer
-        self.param_groups = self.base_optimizer.param_groups
-
-    @torch.no_grad()
-    def first_step(self, zero_grad=False):
-        grad_norm = self._grad_norm()
-        for group in self.param_groups:
-            scale = group["rho"] / (grad_norm + 1e-12)
-
-            for p in group["params"]:
-                if p.grad is None: continue
-                e_w = p.grad * scale.to(p)
-                p.add_(e_w)  # climb to the local maximum "w + e(w)"
-                self.state[p]["e_w"] = e_w
-
-        if zero_grad: self.zero_grad()
-
-    @torch.no_grad()
-    def second_step(self, zero_grad=False):
-        for group in self.param_groups:
-            for p in group["params"]:
-                if p.grad is None: continue
-                p.sub_(self.state[p]["e_w"])  # get back to "w" from "w + e(w)"
-
-        self.base_optimizer.step()  # do the actual "sharpness-aware" update
-
-        if zero_grad: self.zero_grad()
-
-    @torch.no_grad()
-    def step(self, closure=None):
-        assert closure is not None, "Sharpness Aware Minimization requires closure, but it was not provided"
-        closure = torch.enable_grad()(closure)  # the closure should do a full forward-backward pass
-
-        self.first_step(zero_grad=True)
-        closure()
-        self.second_step()
-
-    def _grad_norm(self):
-        shared_device = self.param_groups[0]["params"][
-            0].device  # put everything on the same device, in case of model parallelism
-        norm = torch.norm(
-            torch.stack([
-                p.grad.norm(p=2).to(shared_device)
-                for group in self.param_groups for p in group["params"]
-                if p.grad is not None
-            ]),
-            p=2
-        )
-        return norm
